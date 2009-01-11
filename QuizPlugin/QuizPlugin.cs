6f6176b5263111339f3151af88d00dc9fa7618bf
Apophis <apophis@apophis.ch>
2009-01-11 11:54:27 +0000

    QuizPlugin Base Checkin
    git-svn-id: https://svn.origo.ethz.ch/huffelpuff-irc-bot/trunk@15 b439e43e-95c1-4580-a6c9-a09611c81d30

---
+++ QuizPlugin/QuizPlugin.cs
@@ -0,0 +1,48 @@
+﻿/*
+ * Erstellt mit SharpDevelop.
+ * Benutzer: apophis
+ * Datum: 11.01.2009
+ * Zeit: 12:31
+ * 
+ * Sie können diese Vorlage unter Extras > Optionen > Codeerstellung > Standardheader ändern.
+ */
+using System;
+using System.Collections.Generic;
+
+using Huffelpuff;
+using Huffelpuff.ComplexPlugins;
+
+using Meebey.SmartIrc4net;
+
+namespace Plugin
+{
+    /// <summary>
+    /// Description of QuizPlugin.
+    /// </summary>
+    public class QuizPlugin : AbstractPlugin
+    {
+        public QuizPlugin(IrcBot botInstance) : 
+            base(botInstance) {}
+        
+        public override string Name {
+            get {
+                return "Quiz Bot";
+            }
+        }
+        
+        public override void Activate()
+        {
+            throw new NotImplementedException();
+        }
+        
+        public override void Deactivate()
+        {
+            throw new NotImplementedException();
+        }
+        
+        public override string AboutHelp()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
