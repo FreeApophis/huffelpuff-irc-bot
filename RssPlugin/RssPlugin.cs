d330e8508e8dde29a71e712e8f79d0f1ae930329
Apophis <apophis@apophis.ch>
2009-01-23 01:30:34 +0000

    Added a RssPlugin, should support 2 functionalities:
    TODO:
    * List RssItems
    * Periodically Check for new entries (and announce them)
    
    
    
    git-svn-id: https://svn.origo.ethz.ch/huffelpuff-irc-bot/trunk@23 b439e43e-95c1-4580-a6c9-a09611c81d30

---
+++ RssPlugin/RssPlugin.cs
@@ -0,0 +1,60 @@
+﻿/*
+ *  UT3GlobalStatsPlugin, Access to the GameSpy Stats for UT3
+ * 
+ *  Copyright (c) 2007-2009 Thomas Bruderer <apophis@apophis.ch> <http://www.apophis.ch>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+ */
+ 
+using System;
+using System.Xml;
+using System.Collections.Generic;
+
+using Huffelpuff;
+using Huffelpuff.ComplexPlugins;
+using Meebey.SmartIrc4net;
+
+namespace Plugin
+{
+    /// <summary>
+    /// Description of MyClass.
+    /// </summary>
+    public class RssPlugin : AbstractPlugin
+    {
+        public RssPlugin(IrcBot botInstance) :
+            base(botInstance) {}
+        
+        public override void Activate()
+        {
+            throw new NotImplementedException();
+        }
+        
+        public override void Deactivate()
+        {
+            throw new NotImplementedException();
+        }
+        
+        public override string AboutHelp()
+        {
+            throw new NotImplementedException();
+        }
+        
+        private void getRss() {
+            XmlReader feed = XmlReader.Create("http://forum.vis.ethz.ch/external.php?type=RSS2");
+            while(feed.MoveToElement()){
+                
+            }
+        }
+    }
+}
