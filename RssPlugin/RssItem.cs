841defceb8e183a7739aff379d6e68ff724a1f76
Apophis <apophis@apophis.ch>
2009-01-27 00:13:24 +0000

    Basic RSS Feed Parsing Implemented, !rss Command working
    git-svn-id: https://svn.origo.ethz.ch/huffelpuff-irc-bot/trunk@25 b439e43e-95c1-4580-a6c9-a09611c81d30

---
+++ RssPlugin/RssItem.cs
@@ -0,0 +1,85 @@
+﻿/*
+ * Erstellt mit SharpDevelop.
+ * Benutzer: apophis
+ * Datum: 27.01.2009
+ * Zeit: 00:23
+ * 
+ * Sie können diese Vorlage unter Extras > Optionen > Codeerstellung > Standardheader ändern.
+ */
+using System;
+
+namespace Plugin
+{
+    /// <summary>
+    /// Description of RssItem.
+    /// </summary>
+    public class RssItem
+    {
+        public RssItem(string title, string author, DateTime published, string link, string desc, string category, string content)
+        {
+            this.title = title;    
+            this.author = author;    
+            this.published = published;  
+            this.link = link;
+            this.desc = desc;
+            this.category = category;
+            this.content = content;
+        }
+
+        private string link; 
+        
+        public string Link {
+            get {
+                return link;
+            }
+        }
+        
+        private string desc; 
+        
+        public string Desc {
+            get { 
+                return desc; 
+            }
+        }
+        
+        private string category;
+        
+        public string Category {
+            get { 
+                return category;
+            }
+        }
+        
+        private string content;
+        
+        public string Content {
+            get { 
+                return content;
+            }
+        }
+        
+        private string title;
+            
+        public string Title {
+            get {
+                return title;
+            }
+        }
+
+        private string author;
+
+        public string Author {
+            get {
+                return author;
+            }
+        }
+        
+        private DateTime published;
+
+        public DateTime Published {
+            get {
+                return published;
+            }
+        }
+    }
+}
--- RssPlugin/RssPlugin.cs
+++ RssPlugin/RssPlugin.cs
@@ -37,24 +37,94 @@ namespace Plugin
         
         public override void Activate()
         {
-            throw new NotImplementedException();
+            BotMethods.AddPublicCommand(new Commandlet("!rss", "RSS_HELP", showRss, this));
+            
+            base.Activate();
         }
         
         public override void Deactivate()
         {
-            throw new NotImplementedException();
+            BotMethods.RemovePublicCommand("!rss");
+            
+            base.Deactivate();
         }
         
         public override string AboutHelp()
         {
-            throw new NotImplementedException();
+            return "RSS_PLUGIN_HELP";
         }
         
-        private void getRss() {
-            XmlReader feed = XmlReader.Create("http://forum.vis.ethz.ch/external.php?type=RSS2");
-            while(feed.MoveToElement()){
-                
+        private void showRss(object sender, IrcEventArgs e) {
+            foreach(RssItem item in getRss()) {
+                BotMethods.SendMessage(SendType.Message, e.Data.Channel, item.Title + " was published on " + item.Published.ToString() + " by " + item.Author + " in " + item.Category + " -> " + item.Link);
             }
+        }
+
+        
+        private List<RssItem> getRss() 
+        {
+            List<RssItem> rss = new List<RssItem>();
+
+            XmlReader feed = XmlReader.Create("http://forum.vis.ethz.ch/external.php?type=RSS2");
+            while(feed.Read()){
+                if ((feed.NodeType == XmlNodeType.Element) && (feed.Name == "item")) {
+                    rss.Add(getItem(feed));
+                }
+            }
+            return rss;
+        }
+        
+        private RssItem getItem(XmlReader feed)  {
+
+            string title = "", author = ""; string link = ""; string desc = ""; string category = ""; string content = "";
+
+            DateTime published = DateTime.MinValue;
+                
+            while(feed.Read()){
+                if ((feed.NodeType == XmlNodeType.EndElement) && (feed.Name == "item")) {
+                    break;
+                }
+                if (feed.NodeType == XmlNodeType.Element) {
+                    switch(feed.Name) {
+                        // Main Items every RSS feed has
+                        case "title": feed.Read();
+                            title = feed.ReadContentAsString();
+                        break;
+                        case "link":feed.Read();
+                            link = feed.ReadContentAsString();
+                        break;
+                        case "description":feed.Read();
+                            desc = feed.ReadContentAsString();
+                        break;
+                        // The pubDate is important for notifying
+                        case "pubDate":feed.Read();
+                            published = DateTime.Parse(feed.ReadContentAsString());
+                        break;
+                        // Some more RSS 2.0 Standard fields.
+                        case "category":feed.Read();
+                            category = feed.ReadContentAsString();
+                        break;
+                        case "author":feed.Read();
+                            author = feed.ReadContentAsString();
+                        break;
+                        case "guid":feed.Read();
+                            link = feed.ReadContentAsString();
+                        break;
+                        // Special ones (for vBulletin)
+                        case "content:encoded":feed.Read();
+                            content = feed.ReadContentAsString();
+                        break;
+                        case "dc:creator":feed.Read();
+                            author = feed.ReadContentAsString();
+                        break;
+                        default: Console.WriteLine("unparsed Element: " + feed.Name);
+                        break;
+                    }
+                }
+            }
+
+
+            return new RssItem(title, author, published, link, desc, category, content);
         }
     }
 }
