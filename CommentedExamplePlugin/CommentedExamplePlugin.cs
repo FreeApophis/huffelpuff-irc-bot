6eccd48b98c87e37c343a076c8bbfbf1d3563c40
Apophis <apophis@apophis.ch>
2009-07-25 13:38:34 +0000

    * Started work on the CommentedExamplePlugin, this plugin is for education purposes only
    git-svn-id: https://svn.origo.ethz.ch/huffelpuff-irc-bot/trunk@67 b439e43e-95c1-4580-a6c9-a09611c81d30

--- CommentedExamplePlugin/CommentedExamplePlugin.cs
+++ CommentedExamplePlugin/CommentedExamplePlugin.cs
@@ -1,7 +1,8 @@
 ﻿/*
- *  <project description>
+ *  Commented Example Plugin, as a help for Plugin developers
+ *  ---------------------------------------------------------
  * 
- *  Copyright (c) 2008-2009 Thomas Bruderer <apophis@apophis.ch> 
+ *  Copyright (c) 2008-2009 Thomas Bruderer <apophis@apophis.ch>
  *  File created by apophis at 03.07.2009 18:54
  *
  *  This program is free software: you can redistribute it and/or modify
@@ -15,18 +16,77 @@
  *  GNU General Public License for more details.
  *
  *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 using System;
 using System.Collections.Generic;
 
-namespace CommentedExamplePlugin
+/* 
+ * To write a Plugin you need to set a Reference to the Huffelpuff-project
+ *  If you work with the source, make a new Project for your Plugin and add
+ *  project reference to the Project.
+ *  If you work with the executable directly, Add a reference to the
+ *  Huffelpuff.exe
+ * 
+ *  normally you need the following namesspaces from the project:
+ */
+using Huffelpuff;
+using Huffelpuff.Plugins;
+using Meebey.SmartIrc4net;
+
+
+namespace Plugin
 {
-    /// <summary>
-    /// Description of CommentedExamplePlugin.
-    /// </summary>
-    public class CommentedExamplePlugin
+    /*
+     * This Plugin should be an example to anyone who wants to write a plugin.
+     * It should describe all the main stepts to write an own plugin, and describes
+     * the intentions behinde several design decisions.
+     * 
+     * 1.) A Plugin will run in another Appdomain thant the mainbot, that was done
+     * to make it possible to load, unload and reload Plugins on runtime. This makes
+     * several things a bit difficult.
+     */
+    public class CommentedExamplePlugin : AbstractPlugin
     {
+        
+        /* 
+         * The  constructor should always look like this, the base class Constructor
+         * sets up most part of the infrastructure to use the Bot from a plugin.
+         * It will setup the BotMethods and BotEvents Fields which are the most
+         * important parts for the Plugins.
+         * You dont need to do anything else in the constructor. Whatever you want
+         * to initialize, use Init() for it!
+         */
+        public CommentedExamplePlugin(IrcBot botInstance) :
+            base(botInstance) {}
+        
+        /*
+         * AboutHelp() is the only function you have to override from the 
+         * AbstractPlugin Class. If you implement the above Constructor
+         * and this help Plugin. The Plugin is already able to compile and 
+         * it will show up in the plugins list. If you move the compiled dll
+         * into the plugins folder of your Bot.
+         */
+        public override string AboutHelp()
+        {
+            return "This is the help about the whole CommentedExamplePlugin";
+        }
+        
+        /*
+         * Anything you want to do during initialisation, or what your normally
+         * want to do in a constructor, you do it here!
+         * 
+         * This method is only called once during the lifecylce of the object.
+         * Either when the bot loads the plugins initially, or when all the
+         * plugins get reloaded.
+        */
+        public override void Init()
+        {
+            
+            base.Init();
+        }
+        
+        
         
     }
 }
